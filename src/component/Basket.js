748f60ba2ab65c74645e18419a7941333a3ca10e
porkanokwan <kanokwan.duly@gmail.com>
2022-05-06 15:09:56 +0700

    add component

---
+++ src/component/Basket.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Basket() {
+  return (
+    <div>
+      <h2>Basket</h2>
+    </div>
+  )
+}
+
+export default Basket;
---
+++ src/component/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Product from './Product'
+
+function Main() {
+  return (
+    <div>
+      <h2>Main</h2>
+      <Product/>
+    </div>
+  )
+}
+
+export default Main
